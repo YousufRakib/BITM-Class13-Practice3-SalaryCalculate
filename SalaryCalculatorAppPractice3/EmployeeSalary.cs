e944e4fe3f1700cfba0273a8629dc7336a297c08
YousufRakib <rakibkhanphc@gmail.com>
2019-10-17 01:07:17 +0600

    Practice3

---
+++ SalaryCalculatorAppPractice3/EmployeeSalary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculatorAppPractice3
+{
+    class EmployeeSalary
+    {
+        
+        public double salaryCalculation(double basicSalary,double houseRent,double medicalAllowance)
+        {
+            double salaryCalculate = basicSalary + ((basicSalary * houseRent) / 100) + ((basicSalary * medicalAllowance) / 100);
+            return salaryCalculate;
+        }
+
+    }
+}
---
+++ SalaryCalculatorAppPractice3/SalaryCalculatorUi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryCalculatorAppPractice3
+{
+    public partial class SalaryCalculatorUi : Form
+    {
+        public SalaryCalculatorUi()
+        {
+            InitializeComponent();
+        }
+        EmployeeSalary employeeSalary = new EmployeeSalary();
+        private void ShowMeSalaryButton_Click(object sender, EventArgs e)
+        {
+            string name = employeeNameTextBox.Text;
+            double basicSalary =Convert.ToDouble(basicAmountTextBox.Text);
+            double houseRent = Convert.ToDouble(homeRentTextBox.Text);
+            double medicleAllowance = Convert.ToDouble(medicalAllowanceTextBox.Text);
+            double totalSalary = employeeSalary.salaryCalculation(basicSalary, houseRent, medicleAllowance);
+            MessageBox.Show("Employee Name : " + name + ", Employee Total Salary : "+ totalSalary + " ");
+
+        }
+    }
+}
